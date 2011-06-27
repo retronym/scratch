f1d0c76a01401477f15c15d4a9095bcb0cffa7f0
retronym <jzaugg@gmail.com>
2011-06-27 23:38:35 +0200

    puzzler

--- 20110613/compile.sh
+++ 20110613/compile.sh
@@ -4,3 +4,5 @@ scalac29 -d target unapplyseq.scala
 scala29 -classpath target Main
 scala210 -version
 scala210 -classpath target Main
+
+~/code/scala/build/pack/bin/scala -classpath target Main
--- 20110627/Main.scala
+++ 20110627/Main.scala
@@ -1,3 +1,4 @@
+// Will it run?
 object Main extends App
 
 trait Main
