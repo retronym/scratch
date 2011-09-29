97739fa4057fce0afb4af9107d1c8e26cd2d6d77
Jason Zaugg <jzaugg@gmail.com>
2011-09-29 23:34:27 +0200

    comments

--- 20110929/project/build.scala
+++ 20110929/project/build.scala
@@ -2,7 +2,10 @@ import sbt._
 import Keys._
 
 object build extends Build {
+  // Setting shared by all projects
   lazy val standardSettings = Defaults.defaultSettings ++ Seq(
+    // The `console` analog to `Defaults.mainRunTask` and `Defaults.mainRunMainTask`
+    // It's omission from the standard settings seems to be an oversight. 
     console <<= consoleTask(fullClasspath in Runtime, console)
   )
   
@@ -23,12 +26,14 @@ object build extends Build {
     id = "b",
     base = file("b"),
     settings = standardSettings,
+    // Depend on the `runtime` scope transitively
     dependencies = Seq(a % "compile->compile;runtime->runtime")
   )
 
   import Build.data
   import Project.Initialize
 
+  // Copy/Paste from Defaults.consoleTask, allowing `classpath : ScopedTask` rather than `: Task`.
   def consoleTask(classpath: ScopedTask[Classpath], task: TaskKey[_]): Initialize[Task[Unit]] = {
 
     (compilers in task, classpath, scalacOptions in task, initialCommands in task, streams) map {
